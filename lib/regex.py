90bc4464a3e0988ca953866671e52c524b4786c8
Brian Kaloki <brianngenye@gmail.com>
2025-05-14 08:58:57 +0300

    my first commit

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,13 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
+# Updated name regex
+name = r"^[A-Z][a-z]*(?:['-][A-Z][a-z]*)?(?: [A-Z][a-z]*(?:['-][A-Z][a-z]*)?)?$"
 name_regex = re.compile(name)
 
-phone_number = r""
+# Existing phone number regex is OK
+phone_number = r"^(\(\d{3}\)\s*|\d{3}[-.\s]?)\d{3}[-.\s]?\d{4}$"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+# Updated email regex (starts with a letter)
+email_address = r"^[A-Za-z][\w\.-]*@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
 email_regex = re.compile(email_address)
